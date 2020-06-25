f55ddff511d315698982b82e806a52453abbf1cc
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-06-24 18:16:25 -0700

    ..

--- Index.js
+++ Index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const server = require("./api/server");
 
-const PORT = process.env.PORT || 9000;
+const port = process.env.PORT || 9000;
 
-server.listen(PORT, () => {
-  console.log(`\n* Server listening on Port: ${PORT} *\n`);
+server.listen(port, () => {
+  console.log(`\n* Server listening on Port: ${port} *\n`);
 });
