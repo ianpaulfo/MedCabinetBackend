d09571e68e1819983cd7f6171f4a21e4e626c7e2
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-06-24 21:36:51 -0700

    .

--- api/server.js
+++ api/server.js
@@ -12,11 +12,13 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.use('/api/users', authenticator, usersRouter);
+server.use('/api/auth', authRouter);
+
 server.get("/", (req, res) => {
   res.send(`<h1>Med Cabinet - Backend by Ian Fragoso</h1>`);
 });
 
-server.use('/api/users', authenticator, usersRouter);
-server.use('/api/auth', authRouter);
+
 
 module.exports = server;
